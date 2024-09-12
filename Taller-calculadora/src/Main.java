033da65807de3ee019c8373ac0146692f78ba028
Jenifer97 <jenifer.ayala.a@gmail.com>
2024-09-12 09:34:31 -0500

    Com1

---
+++ Taller-calculadora/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main { public static void main(String[] args) {
+
+    int edad = 15;
+
+    if (edad >= 0 && edad <= 12) {
+        System.out.println("Es niño");
+    } else if (edad >= 13 && edad <= 20) {
+        System.out.println("Es joven");
+    } else if (edad >= 21 && edad <= 60) {
+        System.out.println("Es adulto");
+    } else {
+        System.out.println("Tercera edad");
+    }
+
+}
+}
+
